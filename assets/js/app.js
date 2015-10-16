15a84d0fb8a2cdd9d7ed1873875c5db08367b4e7
Cameron Manley <cameronscode@gmail.com>
2015-10-16 01:46:28 -0500

    Fixed font issue, Added in big landing page img

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,7 @@
 $(document).ready(function(){
-  
+    $("#nav-bottom-row li a").hover(function(){
+        $(this).css("color", "#ff6130");
+        }, function(){
+        $(this).css("color", "");
+    });
 });
