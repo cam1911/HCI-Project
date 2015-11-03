e02fd5048351f7e5b7465c695ed9048a73f77562
Cameron Manley <cameronscode@gmail.com>
2015-11-03 17:54:03 -0600

    Fixed highlighting navbar issue

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
     $("#nav-bottom-row li a").hover(function(){
         $(this).css("color", "#ff6130");
+        $(this).css("background-color", "#4d534c");
         }, function(){
         $(this).css("color", "");
+        $(this).css("background-color", "#4d534c");
     });
     $('#add-review').click(function(){
       var review = prompt("Leave A Review");
